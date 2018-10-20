ab0095ca5de86eee391f01914cfa3a4e8f373db6
cptnyossarian <cptnyossarian2014@gmail.com>
2018-10-21 01:28:36 +0200

    '21.10.2018'

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4371ea36c3eef134892ba050f20bb3e7.js"
+  "/precache-manifest.a9ab36095e809aba742c340a4f79aa04.js"
 );
 
 workbox.clientsClaim();
